61e69531d2083815c72f27d82bbd07241187d507
jdhodges431 <jdhodges431@gmail.com>
2016-06-30 12:45:40 +0100

    disable eslint

--- src/child/containers/App.js
+++ src/child/containers/App.js
@@ -10,12 +10,14 @@ import DevTools from './devTools/DevTools.js';
 import '../../../node_modules/d3fc/dist/d3fc.min.css';
 import '../../../node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css';
 import noFavourites from '../assets/png/no_favourites.png';
+/* eslint-disable import/no-unresolved */
 require('script!../../../node_modules/jquery/dist/jquery.min.js');
 require('script!../../../node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js');
 require('script!../../../node_modules/moment/moment.js');
 require('script!../../../node_modules/BitFlux/node_modules/bootstrap/js/dropdown.js');
 require('script!../../../node_modules/d3fc/dist/d3fc.bundle.min.js');
 require('script!../../../node_modules/BitFlux/dist/bitflux.js');
+/* eslint-enable import/no-unresolved */
 
 const App = ({ code, name, windowState }) => (
     <div className={`main${windowState.isCompact ? ' compact' : ''}`}>
